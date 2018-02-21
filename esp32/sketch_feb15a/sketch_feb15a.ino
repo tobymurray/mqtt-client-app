51f39f08d2f395ba7cf635b964353314b1c96bbc
Toby Murray <murray.toby@gmail.com>
2018-02-20 22:24:48 -0500

    Publish MQTT message using queue

--- esp32/sketch_feb15a/sketch_feb15a.ino
+++ esp32/sketch_feb15a/sketch_feb15a.ino
@@ -1,5 +1,6 @@
 #include <ArduinoJson.h>
 #include <PubSubClient.h>
+#include <cppQueue.h>
 #include <WiFi.h>
 
 #include <LanConfiguration.h>
@@ -16,90 +17,115 @@ const int mqtt_server_port = MQTT_SERVER_PORT;
 const char STATE_OF_HEALTH[] = "stateOfHealth";
 const char DOOR_STATUS[] = "doorStatus";
 
+typedef struct MqttMessage {
+  char topic[128];
+  char body[128];
+};
+MqttMessage mqttMessage;
+
+
 WiFiClient espClient;
 PubSubClient client(espClient);
+// A place to hold messages when MQTT server connection drops
+Queue messageQueue(sizeof(MqttMessage), 10, QueueType::FIFO); 
 long lastMsg = 0;
-char msg[50];
-char json[128];
 int message_number = 0;
 volatile bool oldDoorIsOpen = true;
 volatile bool doorOpen = true;
 
-bool publishWifiNotConnectedMessage(char json[]) {
+bool publishMessage(MqttMessage &mqttMessage) {
+  MqttMessage newMessage;
+  strcpy(newMessage.topic, mqttMessage.topic);
+  strcpy(newMessage.body, mqttMessage.body);
+  return messageQueue.push(&newMessage);
+}
+
+bool publishWifiNotConnectedMessage(MqttMessage &mqttMessage) {
+  strcpy(mqttMessage.topic, STATE_OF_HEALTH);
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& messageBody = jsonBuffer.createObject();
   messageBody["wifiConnectionStatus"] = "notConnected";
-  messageBody.printTo(json, 128);
-  return client.publish(STATE_OF_HEALTH, json);
+  messageBody.printTo(mqttMessage.body, 128);
+  return publishMessage(mqttMessage);
 }
 
-bool publishWifiConnectingMessage(char json[]) {
+bool publishWifiConnectingMessage(MqttMessage &mqttMessage) {
+  strcpy(mqttMessage.topic, STATE_OF_HEALTH);
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& messageBody = jsonBuffer.createObject();
   messageBody["wifiConnectionStatus"] = "connecting";
   messageBody["ssid"] = ssid;
-  messageBody.printTo(json, 128);
-  return client.publish(STATE_OF_HEALTH, json);
+  messageBody.printTo(mqttMessage.body, 128);
+  return publishMessage(mqttMessage);
 }
 
-bool publishWifiConnectedMessage(char json[]) {
+bool publishWifiConnectedMessage(MqttMessage &mqttMessage) {
+  strcpy(mqttMessage.topic, STATE_OF_HEALTH);
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& messageBody = jsonBuffer.createObject();
   messageBody["wifiConnectionStatus"] = "connected";
   messageBody["ipAddress"] = WiFi.localIP().toString();
-  messageBody.printTo(json, 128);
-  return client.publish(STATE_OF_HEALTH, json);
+  messageBody.printTo(mqttMessage.body, 128);
+  return publishMessage(mqttMessage);
 }
 
-bool publishMqttServerConnectingMessage(char json[]) {
+bool publishMqttServerConnectingMessage(MqttMessage &mqttMessage) {
+  strcpy(mqttMessage.topic, STATE_OF_HEALTH);
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& messageBody = jsonBuffer.createObject();
   messageBody["mqttConnectionState"] = "connecting";
-  messageBody.printTo(json, 128);
-  return client.publish(STATE_OF_HEALTH, json);
+  messageBody.printTo(mqttMessage.body, 128);
+  return publishMessage(mqttMessage);
 }
 
-bool publishMqttServerConnectionStateMessage(char json[]) {
+bool publishMqttServerConnectionStateMessage(MqttMessage &mqttMessage) {
+  strcpy(mqttMessage.topic, STATE_OF_HEALTH);
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& messageBody = jsonBuffer.createObject();
   messageBody["mqttConnectionState"] = client.state();
-  messageBody.printTo(json, 128);
-  return client.publish(STATE_OF_HEALTH, json);
+  messageBody.printTo(mqttMessage.body, 128);
+  return publishMessage(mqttMessage);
 }
 
-bool publishDoorStatusChangedMessage(char json[], bool doorOpen) {
+bool publishDoorStatusChangedMessage(MqttMessage &mqttMessage, bool doorOpen) {
+  strcpy(mqttMessage.topic, DOOR_STATUS);
   ++message_number;
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& messageBody = jsonBuffer.createObject();
   messageBody["open"] = doorOpen;
   messageBody["messageNumber"] = message_number;   
-  messageBody.printTo(json, 128);
-  return client.publish(DOOR_STATUS, json, true);
+  messageBody.printTo(mqttMessage.body, 128);
+  return client.publish(mqttMessage.topic, mqttMessage.body, true);
 }
 
 void set_up_wifi() {
-  Serial.print(publishWifiNotConnectedMessage(json));
-  Serial.println(json);
+  Serial.print(publishWifiNotConnectedMessage(mqttMessage));
+  Serial.print(mqttMessage.topic);
+  Serial.println(mqttMessage.body);
 
-  Serial.print(publishWifiConnectingMessage(json));
-  Serial.println(json);
+  Serial.print(publishWifiConnectingMessage(mqttMessage));
+  Serial.print(mqttMessage.topic);
+  Serial.println(mqttMessage.body);
   
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.print(publishWifiConnectingMessage(json));
-    Serial.println(json);
+    Serial.print(publishWifiConnectingMessage(mqttMessage));
+    Serial.print(mqttMessage.topic);
+    Serial.println(mqttMessage.body);
   }
 
-  Serial.print(publishWifiConnectedMessage(json));
-  Serial.println(json);
+  Serial.print(publishWifiConnectedMessage(mqttMessage));
+  Serial.print(mqttMessage.topic);
+  Serial.println(mqttMessage.body);
 }
 
 void reconnect() {
   while (!client.connected()) {
-    Serial.print(publishMqttServerConnectingMessage(json));
-    Serial.println(json);
+    Serial.print(publishMqttServerConnectingMessage(mqttMessage));
+    Serial.print(mqttMessage.topic);
+    Serial.println(mqttMessage.body);
     // Create a random client ID
     String clientId = "ESP32Client-";
     clientId += String(random(0xffff), HEX);
@@ -111,8 +137,9 @@ void reconnect() {
     }
 
     // Publish the connection result
-    Serial.print(publishMqttServerConnectionStateMessage(json));
-    Serial.println(json);
+    Serial.print(publishMqttServerConnectionStateMessage(mqttMessage));
+    Serial.print(mqttMessage.topic);
+    Serial.println(mqttMessage.body);
   }
 }
 
@@ -139,18 +166,36 @@ void setup() {
 
   // Set the initial state of the door
   doorStatusChanged();
-  Serial.print(publishDoorStatusChangedMessage(json, doorOpen));
-  Serial.println(json);
+  Serial.print(publishDoorStatusChangedMessage(mqttMessage, doorOpen));
+  Serial.print(mqttMessage.topic);
+  Serial.println(mqttMessage.body);
+}
+
+void publishQueuedMessage() {
+    MqttMessage oldestMessage;
+    messageQueue.pop(&oldestMessage);
+    Serial.print("  Queued Message: ");
+    Serial.print(client.publish(oldestMessage.topic, oldestMessage.body));
+    Serial.print(oldestMessage.topic);
+    Serial.println(oldestMessage.body);
 }
 
 void loop() {
   if (!client.connected()) {
-    Serial.print(publishMqttServerConnectionStateMessage(json));
-    Serial.println(json);
+    Serial.print(publishMqttServerConnectionStateMessage(mqttMessage));
+    Serial.print(mqttMessage.topic);
+    Serial.println(mqttMessage.body);
     reconnect();
   }
   // Allow the client to process incoming messages and maintain its connection to the server
   client.loop();
+
+  while (!messageQueue.isEmpty()) {
+    Serial.print("There are ");
+    Serial.print(messageQueue.nbRecs());
+    Serial.println(" messages queued up.");
+    publishQueuedMessage();
+  }
 
 
   long now = millis();
@@ -158,8 +203,9 @@ void loop() {
     lastMsg = now;
     
     if (doorOpen != oldDoorIsOpen) {
-      Serial.print(publishDoorStatusChangedMessage(json, doorOpen));
-      Serial.println(json);
+      Serial.print(publishDoorStatusChangedMessage(mqttMessage, doorOpen));
+      Serial.print(mqttMessage.topic);
+      Serial.println(mqttMessage.body);
 
       oldDoorIsOpen = doorOpen;
     }
